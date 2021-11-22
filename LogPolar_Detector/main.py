5ad00017ec64a81bb9ea466a0bbcc3ac08154562
Rakazthas <guillaume.lsn@gmail.com>
2021-11-22 13:18:40 +0100

    cross-power + comments

--- LogPolar_Detector/main.py
+++ LogPolar_Detector/main.py
@@ -12,7 +12,7 @@ import statistics
 from skimage.transform import warp_polar, rotate, rescale
 from skimage.util import img_as_float
 
-blockSize = 64
+blockSize = 8
 logRadius = blockSize/2
 
 imgName = input("Entrer le chemin de l'image : ")
@@ -34,9 +34,14 @@ imgY = cv2.cv2.split(imgYCC)[0]
 blockPerLine = int(w/blockSize)
 blocks = []
 
-for i in range (0, h - blockSize, blockSize):
-    for j in range (0, w - blockSize, blockSize):
-        array = imgY[i:i+blockSize, j:j+blockSize]
+'''
+*************************************************
+creation blocs
+*************************************************
+'''
+for i in range (0, h, blockSize):
+    for j in range (0, w, blockSize):
+        array = imgY[i:i+(blockSize-1), j:j+(blockSize-1)]
         blocks.append(array)
 
 #cv2.imshow("block i", blocks[5 + 5*blockPerLine])
@@ -45,6 +50,11 @@ for i in range (0, h - blockSize, blockSize):
 
 blocksLog = []
 
+'''
+*************************************************
+transformation en coordonnees polaires
+*************************************************
+'''
 for x in blocks:
     imgFl = x.astype(np.float32)
     value = np.sqrt(((imgFl.shape[0]/2.0)**2.0 + (imgFl.shape[1]/2.0)**2.0))
@@ -53,119 +63,43 @@ for x in blocks:
     blocksLog.append(polar_image)
 
 #cv2.imshow("block log", blocksLog[5 + 5*blockPerLine])
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+#if cv2.waitKey(0) & 0xff == 27:
+#    cv2.destroyAllWindows()
 
 
-blocksFFT = []
-
-for x in blocksLog:
-    f = np.fft.fft2(x)
-    blocksFFT.append(f)
-
-'''
-print("Type de f : {}".format(type(f)))
-print("Type de f[0] : {}".format(type(f[0])))
-print("Val de f[0] : {}".format(f[0]))
-'''
 nbBlocks = len(blocksLog)
 L = int(nbBlocks*(nbBlocks-1)/2)
 listEquals = []
 listMax = {}
 listMean = {}
 
+#adv = 1
+'''
+*************************************************
+calcul g et f-1(g)
+*************************************************
+'''
 for i in range(0, nbBlocks):
+    G_a = np.fft.fft2(blocksLog[i])
     for j in range(i+1, nbBlocks):
-        G_a = np.fft.fft2(blocksLog[i])
         G_b = np.fft.fft2(blocksLog[j])
 
         conj_b = np.ma.conjugate(G_b)
 
         R = G_a*conj_b
         R /= np.absolute(R)
-        r = np.fft.ifft2(R)
-        nr = np.absolute(r)
+        r = np.fft.ifft2(R).real
+        #nr = np.absolute(r)
 
-        _max = np.amax(nr)
-        _mean = np.mean(nr)
+        #_max = np.amax(nr)
+        #_mean = np.mean(nr)
 
-        listMax[i, j] = _max
-        listMean[i, j] = _mean
+        listMax[i, j] = np.amax(r) #_max
+        listMean[i, j] = np.mean(r) #_mean
 
-        print("Max du bloc [{}, {}] : {}".format(i, j, _max))
-
-'''
-for i in range(0, len(blocksFFT)):
-    for j in range(i+1, len(blocksFFT)):
-
-        g = numpy.empty(shape=len(blocksFFT[i]))
-        for x in range (0, len(blocksFFT[i])):
-            h = numpy.empty(shape=len(blocksFFT[i][x]))
-            for y in range (0, len(blocksFFT[i][x])):
-                val = blocksFFT[i][x][y] * blocksFFT[j][x][y].conjugate()
-                if val != 0:
-                    val = val/abs(val)
-                np.append(h, val)
-            np.append(g, h)
-
-        _max = []
-        _mean = []
-
-        invG = np.fft.ifft2(g)
-
-        absInvG = []
-
-        for x in invG:
-            absI = []
-            for y in x:
-                absI.append(abs(y))
-            absInvG.append(absI)
-
-        for x in absInvG:
-            _max.append(max(x))
-            _mean.append(statistics.mean(x))
-        finMax = max(_max)
-        finMean = statistics.mean(_mean)
-
-        listMax[i, j] = finMax
-        listMean[i, j] = finMean
-'''
-
-'''
-listMax = {}
-listMean = {}
-for i in range(0, len(blocksLog)):
-    for j in range(i+1, len(blocksLog)):
-        f, cross = scipy.signal.csd(blocksLog[i], blocksLog[j], scaling = 'spectrum')
-
-        _norm = np.linalg.norm(cross)
-        nCross = cross/_norm
-
-        _max = []
-        _mean = []
-
-        invG = np.fft.ifft2(nCross)
-
-        absInvG = []
-
-        for x in invG:
-            absI = []
-            for y in x:
-                absI.append(abs(y))
-            absInvG.append(absI)
-
-        for x in absInvG:
-            _max.append(max(x))
-            _mean.append(statistics.mean(x))
-        finMax = max(_max)
-        finMean = statistics.mean(_mean)
-
-        listMax[i, j] = finMax
-        listMean[i, j] = finMean
-
-        if finMax > 0.35:
-            listEquals.append([i, j])
-'''
+        print("Mean/max du bloc [{}, {}] : {}/{}".format(i, j, listMean[i, j], listMax[i, j]))
+        #print("progression : {}/{}".format(adv, L))
+        #adv += 1
 
 globMeanList = list(listMean.values())
 globMean = np.mean(globMeanList)
@@ -181,7 +115,6 @@ _mean = globMean/L
 '''
 
 
-#globMean = np.mean(listMean)
 '''
 for i in range(0, len(blocksLog)):
     for j in range(i+1, len(blocksLog)):
@@ -189,15 +122,46 @@ for i in range(0, len(blocksLog)):
             listEquals.append([i, j])
 '''
 
+'''
+*************************************************
+test seuil
+*************************************************
+'''
 for key, value in listMean.items():
     print("key : {}, value : {}".format(key, value))
-    if value > 0.35:
+    if value < 0.35:
         listEquals.append(key)
 
+
+'''
+*************************************************
+affichage zones dupliquees
+*************************************************
+'''
+i = 1
+for x in listEquals:
+    blocI = x[0]
+    blocJ = x[1]
+
+    xI = int((blocI % blockPerLine)) * blockSize
+    yI = int((blocI/blockPerLine)) * blockSize
+
+    xJ = int((blocJ % blockPerLine)) * blockSize
+    yJ = int((blocJ/blockPerLine)) * blockSize
+
+    img = cv2.rectangle(img, (xI, yI), (xI + blockSize - 1, yI + blockSize - 1), (0, 0, 255), -1)
+    img = cv2.rectangle(img, (xJ, yJ), (xJ + blockSize - 1, yJ + blockSize - 1), (0, 0, 255), -1)
+    #print("Highlight zone {}/{}".format(i, len(listEquals)))
+    i += 1
+
+cv2.imshow('imgFals', img)
+
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
+
 #print("Moyennes : {}".format(listMean))
-print("Moyenne : {}".format(globMean))
-#TODO : check why diff max/listMax.value
+#print("Moyenne : {}".format(globMean))
 
 
-print("Liste de couples : {}".format(listEquals))
+#print("Liste de couples : {}".format(listEquals))
 
