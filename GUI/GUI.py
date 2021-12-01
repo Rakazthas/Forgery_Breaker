262874cb85beb0adefdb6b5c0c66aa5d1c874034
Rakazthas <guillaume.lsn@gmail.com>
2021-12-01 08:34:26 +0100

    sift V1

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,24 +1,52 @@
 from tkinter import *
 from tkinter.filedialog import *
-from tkinter.messagebox import showerror
+from tkinter.messagebox import showerror, showinfo
 import cv2.cv2 as cv2
+
+from siftDetector import SIFT
 
 '''
 import les scripts de test
 '''
 global img
 
+
 def openImg():
     res = askopenfilename(title="Ouvrir une image", filetypes=[('all files', '.*')])
     filePath.delete(0, END)
     filePath.insert(0, res)
+
 
 def testForgery():
     img = cv2.imread(filename=path.get())
     if img is None:
         showerror("Erreur", "Veuillez entrer un chemin d'image valide")
     else:
-        print("test en cours")
+        cv2.imshow("Image à tester", img)
+        print("Test en cours")
+
+        if epsVar.get() > 500:
+            epsVar.set(500)
+        if epsVar.get() <= 0:
+            epsVar.set(1)
+
+        if minSamplesVar.get() > 50:
+            minSamplesVar.set(50)
+        if minSamplesVar.get() < 2:
+            minSamplesVar.set(2)
+
+        detect = SIFT(img=img)
+        detect.siftFind()
+
+        features = detect.siftShowFeatures()
+        cv2.imshow("Features", features)
+
+        forgery = detect.forgeryLocate(eps=float(epsVar.get()), minSamples=float(minSamplesVar.get()))
+        if forgery is None:
+            showinfo("Résultat", "Aucune falsification copy-move détectée")
+        else:
+            cv2.imshow("Falsification trouvée", forgery)
+
 
 window = Tk()
 
@@ -37,14 +65,16 @@ imageWindow.pack()
 
 optionsWindow = PanedWindow(window, orient=HORIZONTAL)
 
-eps = Spinbox(optionsWindow, from_=0, to=500)
-minSamples = Spinbox(optionsWindow, from_=0, to=50)
+epsVar = DoubleVar(40)
+minSamplesVar = DoubleVar(2)
+
+eps = Spinbox(optionsWindow, from_=1, to=500, textVariable=epsVar)
+minSamples = Spinbox(optionsWindow, from_=2, to=50, textVariable=minSamplesVar)
 
 optionsWindow.add(eps)
 optionsWindow.add(minSamples)
 
 optionsWindow.pack()
-
 
 launchButton = Button(window, text="Lancer le test", command=testForgery)
 launchButton.pack(side=BOTTOM)
--- copy-move_detector/siftDetector.py
+++ copy-move_detector/siftDetector.py
@@ -7,10 +7,13 @@ class SIFT(object):
     def __init__(self, filepath):
         self.img = cv2.imread(filepath)
 
+    def __init__(self, img):
+        self.img = img
+
     def siftFind(self):
         sift =cv2.xfeatures2d.SIFT_create()
         grayScaleImg = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-        self.keyPts, self.descriptors = sift.detectAndCompute(gray, none)
+        self.keyPts, self.descriptors = sift.detectAndCompute(grayScaleImg, None)
 
     def siftShowFeatures(self):
         siftImg = cv2.drawKeypoints(self.img, self.keyPts, self.img.copy())
@@ -19,19 +22,19 @@ class SIFT(object):
     def forgeryLocate(self, eps=40, minSamples=2):
         clusters = DBSCAN(eps=eps, min_samples=minSamples).fit(self.descriptors)
         size = np.unique(clusters.labels_).shape[0] - 1
-        forgery = slef.img.copy()
-        if size == 0 & np.unique(clusters.labels_)[0] == -1:
+        forgery = self.img.copy()
+        if (size == 0) and (np.unique(clusters.labels_)[0] == -1):
             print("No forgery found")
             return None
         else:
             if size == 0:
                 size=1
-        clustersList=[[] for i in range(size)]
-        for i in range(len(self.keyPts)):
-            if clusters.labels_[i] != -1:
-                clustersList[clusters.labels_[i]].append((int(self.keyPts[i].pt[0]), int(self.keyPts[i].pt[1])))
-        for pts in clustersList:
-            if len(pts) > 1:
-                for i in range(1, len(pts)):
-                    cv2.line(forgery, pts[0], pts[i], (0,0,255), 2)
-        return forgery
+            clustersList=[[] for i in range(size)]
+            for i in range(len(self.keyPts)):
+                if clusters.labels_[i] != -1:
+                    clustersList[clusters.labels_[i]].append((int(self.keyPts[i].pt[0]), int(self.keyPts[i].pt[1])))
+            for pts in clustersList:
+                if len(pts) > 1:
+                    for i in range(1, len(pts)):
+                        cv2.line(forgery, pts[0], pts[i], (0,0,255), 3)
+            return forgery
